00ba5718175862e1d0531d0987fe647f6a31c3e8
sisilhowie99 <sisil.anastasia@gmail.com>
2022-02-02 02:09:09 +1000

    Tile complete
    Format TileList file

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-export const Tile = () => {
-  return (
-    <div className="tile-container">
-      
-    </div>
-  );
+export const Tile = ({ object }) => {
+	return (
+		<div className="tile-container">
+			{Object.values(object).map((obj, index) => (
+				<p className={index === 0 ? "tile-title" : "tile"} key={index}>
+					{obj}
+				</p>
+			))}
+		</div>
+	);
 };
--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { Tile } from '../tile/Tile';
+import { Tile } from "../tile/Tile";
 
 export const TileList = ({ data }) => {
-  return (
-    <div>
-      {data.map((object, index) => <Tile object={object} key={index} />)}
-    </div>
-  );
+	return (
+		<div>
+			{data.map((object, index) => (
+				<Tile object={object} key={index} />
+			))}
+		</div>
+	);
 };
