856f758fc38168d6416aad174580f2762066d822
sisilhowie99 <sisil.anastasia@gmail.com>
2022-02-02 02:18:40 +1000

    AppointmentsPage complete
    Pass contacts array to the component from App component level

--- src/App.js
+++ src/App.js
@@ -44,7 +44,11 @@ function App() {
 					</Route>
 					<Route path={ROUTES.APPOINTMENTS}>
 						{/* Add props to AppointmentsPage */}
-						<AppointmentsPage appointments={appointments} addAppointment={addAppointment} />
+						<AppointmentsPage
+							contacts={contacts}
+							appointments={appointments}
+							addAppointment={addAppointment}
+						/>
 					</Route>
 				</Switch>
 			</main>
--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,28 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
+import { TileList } from "../../components/tileList/TileList";
 
-export const AppointmentsPage = () => {
-  /*
-  Define state variables for 
-  appointment info
-  */
+export const AppointmentsPage = ({ contacts, appointments, addNewAppointment }) => {
+	const [title, setTitle] = useState("");
+	const [contact, setContact] = useState("");
+	const [date, setDate] = useState("");
+	const [time, setTime] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    /*
-    Add contact info and clear data  
-    */
-   
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		addNewAppointment(title, contact, date, time);
+		setTitle("");
+		setContact("");
+		setDate("");
+		setTime("");
+	};
 
-  return (
-    <div>
-      <section>
-        <h2>Add Appointment</h2>
-      </section>
-      <hr />
-      <section>
-        <h2>Appointments</h2>
-      </section>
-    </div>
-  );
+	return (
+		<div>
+			<section>
+				<h2>Add Appointment</h2>
+				<AppointmentForm
+					title={title}
+					setTitle={setTitle}
+					contact={contact}
+					setContact={setContact}
+					date={date}
+					setDate={setDate}
+					time={time}
+					setTime={setTime}
+          onSubmit={handleSubmit}
+				/>
+			</section>
+			<hr />
+			<section>
+				<h2>Appointments</h2>
+        <TileList data={appointments} />
+			</section>
+		</div>
+	);
 };
